6401d61fbd26b5798a7f80672309845701f9acb8
evertwh <e.haasdijk@vu.nl>
2017-05-18 14:02:46 +0200

    implemented Diversity babbling

--- MDB_SocialLearning/FitnessLibrary.hpp
+++ MDB_SocialLearning/FitnessLibrary.hpp
@@ -3,35 +3,24 @@
 
 #include <string>
 #include "GeneticAlgorithm.hpp"
-#include "../Experiments/FastSim_Phototaxis.h"
-#include "../Experiments/XOR_Experiment.h"
 #include "../Experiments/FastSim_MultiRobot_WallAvoidance.h"
-#include "../Experiments/FastSim_Phototaxis_DistanceFit.h"
 #include "../Experiments/FastSim_Phototaxis_Compass.h"
 #include "../Experiments/FastSim_MultiRobot_Phototaxis_Compass.h"
 #include "../Experiments/FastSim_Forage_Wall.h"
+#include "../Experiments/FastSim_Diversity_Compass.h"
 
 namespace MDB_Social {
 
 	 class FitnessLibrary {
 	 private:
-		 static GAFitness* getFastSim_Phototaxis(std::string id) {
-			 return new FastSim_Phototaxis(id);
-		}
-		 static GAFitness* getXOR_Experiment(std::string id) {
-			 return new XOR_Experiment(id);
-		}
 		 static GAFitness* getFastSim_MultiRobot_WallAvoidance(std::string id) {
 			 return new FastSim_MultiRobot_WallAvoidance(id);
-		}
-		 static GAFitness* getFastSim_Phototaxis_DistanceFit(std::string id) {
-			 return new FastSim_Phototaxis_DistanceFit(id);
 		}
 		 static GAFitness* getFastSim_Phototaxis_Compass(std::string id) {
 			 return new FastSim_Phototaxis_Compass(id);
 		}
-		 static GAFitness* getFastSim_MultiRobot_Phototaxis_Compass(std::string id) {
-			 return new FastSim_MultiRobot_Phototaxis_Compass(id);
+		 static GAFitness* getFastSim_Diversity_Compass(std::string id) {
+			 return new FastSim_Diversity_Compass(id);
 		}
 		 static GAFitness* getFastSim_Forage_Wall(std::string id) {
 			 return new FastSim_Forage_Wall(id);
@@ -39,20 +28,16 @@ namespace MDB_Social {
 
 	 public:
 		 static GAFitness* getFitness(std::string fit, std::string id="Default") {
-			 if (fit == "FastSim_Phototaxis")
-				 return getFastSim_Phototaxis(id);
-			 else if (fit == "XOR_Experiment")
-				 return getXOR_Experiment(id);
-			 else if (fit == "FastSim_MultiRobot_WallAvoidance")
+			 if (fit == "FastSim_MultiRobot_WallAvoidance")
 				 return getFastSim_MultiRobot_WallAvoidance(id);
-			 else if (fit == "FastSim_Phototaxis_DistanceFit")
-				 return getFastSim_Phototaxis_DistanceFit(id);
 			 else if (fit == "FastSim_Phototaxis_Compass")
 				 return getFastSim_Phototaxis_Compass(id);
 			 else if (fit == "FastSim_MultiRobot_Phototaxis_Compass")
 				 return getFastSim_MultiRobot_Phototaxis_Compass(id);
 			 else if (fit == "FastSim_Forage_Wall")
 				 return getFastSim_Forage_Wall(id);
+			 else if (fit == "FastSim_Diversity_Compass")
+				 return getFastSim_Diversity_Compass(id);
 			 else
 				 return NULL;
 		 }
